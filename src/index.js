fa6941d353b71b0d337b5d5c5e61e3266b4a1cc5
Scott Morse <scottmorsedev@gmail.com>
2019-01-06 12:33:22 -0600

    Mosca server now hosts MQTT broker directly on the app

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 const app = require('express')()
 const RED = require('node-red')
 const http = require('http')
+const mosca = require('mosca')
 
-const redSettings = require('./red/settings.json')
+const color = require('./utils/colorWrap')
 
+//Node server
 const server = http.createServer(app)
+const port = process.env.PORT || 3000
+
+//Mosca
+const moscaSettings = {
+  port: 1883,
+  host: '127.0.0.1',
+  //backend: // ascoltatore settings for DB
+}
+
+const moscaServer = new mosca.Server(moscaSettings)
+moscaServer.on('ready', ()=>console.log(moscaServer));
+
+//Node-Red
+const redSettings = require('./red/settings.json')
 
 RED.init(server,redSettings)
 
@@ -12,8 +28,6 @@ app.use(redSettings.httpAdminRoot,RED.httpAdmin)
 
 app.use(redSettings.httpNodeRoot,RED.httpNode)
 
-const port = process.env.PORT || 3000
-
+//starts
 server.listen(port,()=>console.log('Server Listening on port ' + port))
-
 RED.start().then(()=>require('./mosquittos/index'))
